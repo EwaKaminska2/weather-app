10b4435c155c7317c8baa01d80792f4906850cc8
Uzytkownik <uzytkownik@kursGIT.pl>
2020-10-04 11:49:28 +0200

    weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StartComponent } from './component/start/start.component';
-import { SelectCityComponent } from './component/select-city/select-city.component';
-import { ShowWeatherComponent } from './component/show-weather/show-weather.component';
-import { ToolbarComponent } from './component/toolbar/toolbar.component';
-import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {StartComponent} from './component/start/start.component';
+import {SelectCityComponent} from './component/select-city/select-city.component';
+import {ShowWeatherComponent} from './component/show-weather/show-weather.component';
+import {ToolbarComponent} from './component/toolbar/toolbar.component';
+import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-
-const materialModules = [MatToolbarModule, MatButtonModule, MatIconModule];
-
-
-
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+const materialModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,10 +34,13 @@ const materialModules = [MatToolbarModule, MatButtonModule, MatIconModule];
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule ,
-    ...materialModules
+    BrowserAnimationsModule,
+    ...materialModules,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
